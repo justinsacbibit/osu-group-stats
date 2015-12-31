cecf95eb0cae6e8546f2481a9ca841c7f04acaae
Justin Sacbibit <jsacbibit@gmail.com>
2015-12-31 15:52:44 -0500

    Prepare for hex publish

--- config/config.exs
+++ config/config.exs
@@ -7,7 +7,5 @@ config :quantum, cron: [
 
 config :logger, level: :info
 
-config :uw_osu_stat, osu_api_key: System.get_env("OSU_API_KEY")
-
 import_config "#{Mix.env}.exs"
 
--- mix.exs
+++ mix.exs
@@ -5,6 +5,8 @@ defmodule UwOsuStat.Mixfile do
     [app: :uw_osu_stat,
      version: "0.0.1",
      elixir: "~> 1.1",
+     description: description,
+     package: package,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -15,6 +17,9 @@ defmodule UwOsuStat.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [mod: {UwOsuStat.App, []},
+     env: [
+       osu_api_key: System.get_env("OSU_API_KEY"),
+     ],
      applications: [
        :logger,
        :postgrex,
@@ -22,6 +27,19 @@ defmodule UwOsuStat.Mixfile do
        :quantum,
        :httpoison,
      ]]
+  end
+
+  defp description do
+    """
+    Use the osu! API to store data for a set of players.
+    """
+  end
+
+  defp package do
+    [files: ["lib", "priv", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Justin Sacbibit"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/justinsacbibit/uw-osu-stat"}]
   end
 
   # Dependencies can be Hex packages:
@@ -39,6 +57,7 @@ defmodule UwOsuStat.Mixfile do
      {:quantum, "~> 1.6.1"},
      {:httpoison, "~> 0.8.0"},
      {:poison, "~> 1.5"},
-     {:mock, "~> 0.1.1", only: :test}]
+     {:mock, "~> 0.1.1", only: :test},
+     {:mix_test_watch, "~> 0.2", only: :dev}]
   end
 end
