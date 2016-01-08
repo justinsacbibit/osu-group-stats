47a10762da8fec84763aa6bbab5eea28f97b753c
Justin Sacbibit <jsacbibit@gmail.com>
2016-01-08 05:03:20 -0500

    Update prod hostname

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :uw_osu, UwOsu.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "uw_osu.sacbibit.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
