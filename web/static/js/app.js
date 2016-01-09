c81cfb1271e4134273106bc04f97ebc91d88553f
Justin Sacbibit <jsacbibit@gmail.com>
2016-01-09 02:44:18 -0500

    Add farmed beatmaps tables

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -7,38 +7,144 @@ import TableHeader from 'material-ui/lib/table/table-header';
 import TableHeaderColumn from 'material-ui/lib/table/table-header-column';
 import TableRow from 'material-ui/lib/table/table-row';
 import TableRowColumn from 'material-ui/lib/table/table-row-column';
+import Paper from 'material-ui/lib/paper';
 
 
 export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      beatmaps: [],
+      selectedBeatmapIndex: null,
+    };
+  }
+
   componentDidMount() {
     const root = location.protocol + '//' + location.host;
-    $.get(`${root}/api/weekly-snapshots`, snapshots => {
+    $.get(`${root}/api/farmed-beatmaps`, beatmaps => {
       this.setState({
-        snapshots,
+        beatmaps,
       });
     });
+  }
+
+  handleOnRowSelection(selectedRows) {
+    if (selectedRows.length === 1) {
+      const [ selectedRow ] = selectedRows;
+      this.setState({
+        selectedBeatmapIndex: selectedRow,
+      });
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>This app is hot!</h1>
-        <RaisedButton label='Hi!' />
-        <Table>
-          <TableHeader displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn>
-              </TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableRowColumn>
-                agsfgsdg
-              </TableRowColumn>
-            </TableRow>
-          </TableBody>
-        </Table>
+        <Paper style={{ display: 'inline-block', width: '800px' }}>
+          <Table
+            height='500px'
+            onRowSelection={this.handleOnRowSelection.bind(this)}>
+            <TableHeader
+              displaySelectAll={false}>
+              <TableRow>
+                <TableHeaderColumn style={{ width: '20px' }}>
+                  Rank
+                </TableHeaderColumn>
+                <TableHeaderColumn style={{ width: '20px' }}>
+                  # Scores
+                </TableHeaderColumn>
+                <TableHeaderColumn>
+                  Artist
+                </TableHeaderColumn>
+                <TableHeaderColumn>
+                  Title
+                </TableHeaderColumn>
+                <TableHeaderColumn>
+                  Version
+                </TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {this.state.beatmaps.map((beatmap, index) => {
+                return (
+                  <TableRow
+                    key={index}>
+                    <TableRowColumn style={{ width: '20px' }}>
+                      {index + 1}
+                    </TableRowColumn>
+                    <TableRowColumn style={{ width: '20px' }}>
+                      {beatmap.scores.length}
+                    </TableRowColumn>
+                    <TableRowColumn>
+                      {beatmap.artist}
+                    </TableRowColumn>
+                    <TableRowColumn>
+                      {beatmap.title}
+                    </TableRowColumn>
+                    <TableRowColumn>
+                      {beatmap.version}
+                    </TableRowColumn>
+                  </TableRow>
+                  );
+              })}
+            </TableBody>
+          </Table>
+        </Paper>
+
+        {this.state.selectedBeatmapIndex !== null ?
+          (() => {
+            const selectedBeatmap = this.state.beatmaps[this.state.selectedBeatmapIndex];
+
+            return (
+              <Paper style={{ marginLeft: '50px', width: '500px', display: 'inline-block' }}>
+                <Table height='500px'>
+                  <TableHeader
+                    adjustForCheckbox={false}
+                    displaySelectAll={false}>
+                    <TableRow>
+                      <TableHeaderColumn style={{ width: '20px' }}>
+                        Rank
+                      </TableHeaderColumn>
+                      <TableHeaderColumn style={{ width: '70px' }}>
+                        Username
+                      </TableHeaderColumn>
+                      <TableHeaderColumn style={{ width: '50px' }}>
+                        PP
+                      </TableHeaderColumn>
+                      <TableHeaderColumn>
+                        Date
+                      </TableHeaderColumn>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody
+                    adjustForCheckbox={false}
+                    displayRowCheckbox={false}>
+                    {selectedBeatmap.scores.map((score, index) => {
+                      const scoreDate = new Date(score.date);
+                      return (
+                        <TableRow
+                          key={index}>
+                          <TableRowColumn style={{ width: '20px' }}>
+                            {index + 1}
+                          </TableRowColumn>
+                          <TableRowColumn style={{ width: '70px' }}>
+                            {score.user.username}
+                          </TableRowColumn>
+                          <TableRowColumn style={{ width: '50px' }}>
+                            {score.pp}
+                          </TableRowColumn>
+                          <TableRowColumn>
+                            {scoreDate.toLocaleString('en-US')}
+                          </TableRowColumn>
+                        </TableRow>
+                        );
+                    })}
+                  </TableBody>
+                </Table>
+              </Paper>
+            );
+          })()
+        : null}
       </div>
     )
   }
