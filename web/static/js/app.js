ca8c44eab8ff1fce0156a38e107b395e5574c5e3
Justin Sacbibit <jsacbibit@gmail.com>
2016-01-12 02:48:32 -0500

    Update players table with sortable columns

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -10,6 +10,7 @@ import TableRowColumn from 'material-ui/lib/table/table-row-column';
 import Paper from 'material-ui/lib/paper';
 import LeftNav from 'material-ui/lib/left-nav';
 import MenuItem from 'material-ui/lib/menus/menu-item';
+import classNames from 'classnames';
 
 
 class Beatmaps extends Component {
@@ -142,62 +143,125 @@ class Beatmaps extends Component {
 
 
 class Players extends Component {
-  shouldComponentUpdate(nextProps) {
-    return nextProps.players !== this.props.players;
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      sortIndex: 0,
+      sortDirection: 'descending',
+    };
+  }
+
+  handleOnClickSortableColumn(index) {
+    if (index === this.state.sortIndex) {
+      if (this.state.sortDirection === 'ascending') {
+        this.setState({
+          sortDirection: 'descending',
+        });
+      } else {
+        this.setState({
+          sortDirection: 'ascending',
+        });
+      }
+    } else {
+      this.setState({
+        sortDirection: 'descending',
+        sortIndex: index,
+      });
+    }
+  }
+
+  _sortPlayers(p1, p2) {
+    const columnMap = [
+      [p => p.pp_raw, -1],
+      [p => p.pp_rank, 1],
+      [p => p.pp_country_rank, 1],
+      [p => p.playcount, -1],
+      [p => p.accuracy, -1],
+    ];
+    const column = columnMap[this.state.sortIndex];
+    let d1 = column[0](p1);
+    let d2 = column[0](p2);
+    if (this.state.sortDirection === 'ascending') {
+      const temp = d1;
+      d1 = d2;
+      d2 = temp;
+    }
+    if (column[1] === 1) {
+      const temp = d1;
+      d1 = d2;
+      d2 = temp;
+    }
+    return d2 - d1;
   }
 
   render() {
+    const sortableColumns = [
+      'PP',
+      'PP Rank',
+      'Country Rank',
+      'Playcount',
+      'Accuracy',
+    ];
+    const players = this.props.players.slice();
+    players.sort(this._sortPlayers.bind(this));
     return (
-      <Paper style={{ margin: '40px 130px', width: '700px' }}>
-        <Table
-          height='500px'>
-          <TableHeader
-            adjustForCheckbox={false}
-            displaySelectAll={false}>
-            <TableRow>
-              <TableHeaderColumn style={{ width: '20px' }}>
-                Rank
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '100px' }}>
-                Username
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '100px' }}>
-                PP
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '100px' }}>
-                PP Rank
-              </TableHeaderColumn>
-              <TableHeaderColumn style={{ width: '100px' }}>
-                Country Rank
-              </TableHeaderColumn>
-            </TableRow>
-          </TableHeader>
-          <TableBody displayRowCheckbox={false}>
-            {this.props.players.map((player, index) => {
+      <table className='ui sortable celled table'>
+        <thead>
+          <tr>
+            <th>
+              Rank
+            </th>
+            <th>
+              Username
+            </th>
+            {sortableColumns.map((header, index) => {
+              const sorted = index === this.state.sortIndex;
               return (
-                <TableRow
-                  key={index}>
-                  <TableRowColumn style={{ width: '20px' }}>
-                    {index + 1}
-                  </TableRowColumn>
-                  <TableRowColumn style={{ width: '100px' }}>
-                    {player.username}
-                  </TableRowColumn>
-                  <TableRowColumn style={{ width: '100px' }}>
-                    {player.pp_raw}
-                  </TableRowColumn>
-                  <TableRowColumn style={{ width: '100px' }}>
-                    {player.pp_rank}
-                  </TableRowColumn>
-                  <TableRowColumn style={{ width: '100px' }}>
-                    {player.pp_country_rank}
-                  </TableRowColumn>
-                </TableRow>
-                );
+                <th
+                  className={classNames(sorted, {
+                    'sorted': sorted,
+                    [this.state.sortDirection]: sorted,
+                  })}
+                  key={index}
+                  onClick={this.handleOnClickSortableColumn.bind(this, index)}>
+                  {header}
+                </th>
+              );
             })}
-          </TableBody>
-        </Table>
-      </Paper>
+          </tr>
+        </thead>
+        <tbody>
+          {players.map((player, index) => {
+            return (
+              <tr
+                key={index}>
+                <td>
+                  {index + 1}
+                </td>
+                <td>
+                  {player.username}
+                </td>
+                <td>
+                  {player.pp_raw}
+                </td>
+                <td>
+                  {player.pp_rank}
+                </td>
+                <td>
+                  {player.pp_country_rank}
+                </td>
+                <td>
+                  {player.playcount}
+                </td>
+                <td>
+                  {player.accuracy.toFixed(2)}
+                </td>
+              </tr>
+              );
+          })}
+        </tbody>
+      </table>
     );
   }
 }
@@ -244,11 +308,6 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <LeftNav open width={100}>
-          <MenuItem onTouchTap={this.handleOnTouchTapBeatmaps.bind(this)}>Beatmaps</MenuItem>
-          <MenuItem onTouchTap={this.handleOnTouchTapPlayers.bind(this)}>Players</MenuItem>
-        </LeftNav>
-
         <Beatmaps
           beatmaps={this.state.beatmaps}
           onRowSelection={this.handleOnRowSelection.bind(this)}
