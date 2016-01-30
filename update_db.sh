3b911b58673e336388d6e909529066835240d94a
Justin Sacbibit <jsacbibit@gmail.com>
2016-01-30 18:49:19 -0500

    Update update_db ssh command

--- update_db.sh
+++ update_db.sh
@@ -1,4 +1,4 @@
-ssh uw dokku postgres:export uw_osu_stat > uw_osu_dump
+ssh uw "dokku postgres:export uw_osu_stat > uw_osu_dump"
 scp uw:/root/uw_osu_dump .
 mix do ecto.drop, ecto.create
 pg_restore -d uw_osu_dev uw_osu_dump
